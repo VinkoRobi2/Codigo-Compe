307259a1d2fc8681e38101e64b137d0d47d4de2c
VinkoRobi2 <161661761+VinkoRobi2@users.noreply.github.com>
2024-06-17 15:43:30 -0500

    Update Simondice.hh

--- SimonDice/Simondice.hh
+++ SimonDice/Simondice.hh
@@ -1,225 +1,101 @@
-#include "pitches.h"
-#define LEDROJO 0
-#define LEDAZUL 1
-#define LEDVERDE 2
-#define LEDAMARILLO 3
-#define NUMLEDS 4 
-#define TOTAL 32 
+#define LED_RED 0
+#define LED_GREEN 2
+#define LED_BLUE 4
+#define LED_YELLOW 6
 
-int leds[NUMLEDS] =    {2, 4, 6, 8};
-int botones[NUMLEDS] = {3, 5, 7, 9};
-int buzz = 10;
+#define NUM_LEDS 4
+#define SEQUENCE_LENGTH 32 // Maximum sequence length (adjust as needed)
 
-int partida[TOTAL], nivelActual = 0, nivelIndice = 0;
+int leds[NUM_LEDS] = {LED_RED, LED_GREEN, LED_BLUE, LED_YELLOW};
+int buttons[NUM_LEDS] = {3, 5, 7, 9};
 
+int sequence[SEQUENCE_LENGTH];  // Stores the sequence of colors
+int currentLevel = 0;           // Current level (sequence length)
+int sequenceIndex = 0;          // Index for iterating through sequence
 
-int jugada = 0;
+bool isNewGame = true;
 
-
-int nuevoJuego=1;
-
-
-int melody[] = {
-NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
-};
-
-int noteDurations[] = {
-4, 8, 8, 4, 4, 4, 4, 4
-};
-
-void setup() 
-{
-  int i;
-
-  for (i=0; i<NUMLEDS; i++)
-  {
+void setup() {
+  for (int i = 0; i < NUM_LEDS; i++) {
     pinMode(leds[i], OUTPUT);
-    pinMode(botones[i], INPUT);
-  }    
-  
-  pinMode(buzz, OUTPUT);
-  randomSeed(analogRead(0));  
-  Serial.begin(9600);
+    pinMode(buttons[i], INPUT_PULLUP); // Use pull-up resistors for buttons
+  }
+  randomSeed(analogRead(0));
 }
 
-void loop() 
-{               
-    if (nuevoJuego)
-    {
-      llenarPartida();  
-      nivelActual=0;
-      nuevoJuego = 0;            
-      ejecutarMelodia();
+void loop() {
+  if (isNewGame) {
+    generateSequence();
+    currentLevel = 0;
+    sequenceIndex = 0;
+    displaySequence(currentLevel);
+    isNewGame = false;
+  }
+
+  // Add visual feedback like a bar sweep or flashing LEDs here (optional)
+
+  while (!isNewGame && sequenceIndex <= currentLevel) {
+    Serial.println("Reading buttons...");
+    Serial.println(sequence[sequenceIndex]);
+
+    int buttonPressed = readButtons();
+    Serial.println(buttonPressed);
+
+    lightLED(buttonPressed);
+    delay(250);
+    turnOffLED(buttonPressed);
+
+    if (sequence[sequenceIndex] != buttonPressed) {
+      Serial.println("Game Over");
+      Serial.print("Level: ");
+      Serial.println(currentLevel);
+      isNewGame = true;
+    } else {
+      sequenceIndex++;
+      delay(250);
     }
-    fxBarrido(30, 2);
-    mostrarPartida(nivelActual, 300);
+  }
 
-    while (!nuevoJuego && nivelIndice<=nivelActual) 
-    {
-      Serial.println("leyendo botones");
-      Serial.println(partida[nivelIndice]);
-      jugada = leerBotones();
-      Serial.println(jugada);
-      encender(jugada);
-      delay(250);  
-      apagar(jugada);    
-      
-      if (partida[nivelIndice] != jugada)
-      {       
-        Serial.println("Game Over");
-        Serial.print("nivel ");
-        Serial.println(nivelActual);
-        nuevoJuego = 1;  
-        fxBaliza(300, 4);               
-      }   
-      else
-      { 
-        nivelIndice++; 
-        delay(250);                     
-      }
-    }                  
-    nivelIndice = 0;
-    nivelActual++;
-}
-
-int encender(int led)
-{
-    digitalWrite(leds[led], 1);
-}
-
-
-int apagar(int led)
-{
-    digitalWrite(leds[led], 0);
-}
-
-void encenderTodos(){
-  int i;
-  for (i=0; i<NUMLEDS; i++)
-  {
-    digitalWrite(leds[i], 1);
-  }  
-}
-
-void apagarTodos()
-{
-  int i;
-  for (i=0; i<NUMLEDS; i++)
-  {
-    digitalWrite(leds[i], 0);
+  if (sequenceIndex > currentLevel) {
+    // Player completed the level, move to the next one
+    currentLevel++;
+    sequenceIndex = 0;
   }
 }
 
-
-void fxBarrido(int demora, int repeticiones)
-{
-  int i, j;
-  apagarTodos();
-  for (i=0; i<=repeticiones; i++)
-  {
-      for (j=0; j<NUMLEDS; j++)
-      {
-        encender(j);
-        if (j != 0)
-        {
-            apagar (j-1);            
-        }
-        delay(demora);
-      }  
-      for (j=NUMLEDS-2; j>=0; j--)
-      {    
-        encender(j);
-        apagar(j+1);
-        delay(demora);    
-      }
+void generateSequence() {
+  for (int i = 0; i < SEQUENCE_LENGTH; i++) {
+    sequence[i] = random(NUM_LEDS);
   }
-  apagarTodos();
 }
 
-
-void fxBaliza(int demora, int repeticiones)
-{
-    int i;
-    for (i=0; i<=repeticiones; i++)
-    {
-        apagarTodos();
-        delay(demora);
-        encenderTodos();
-        delay(demora);
-    }
-    apagarTodos();
-    delay(demora);
+void displaySequence(int length) {
+  for (int i = 0; i <= length; i++) {
+    delay(300);
+    lightLED(sequence[i]);
+    delay(300);
+    turnOffLED(sequence[i]);
+  }
 }
 
-
-void llenarPartida()
-{  
-    int i;
-    for (i=0; i<TOTAL; i++)
-    {
-        partida[i] = random(NUMLEDS);  
-    }
-}
-
-
-void mostrarPartida(int hasta, int demora)
-{
-  int i;
-  for (i=0; i<=hasta; i++)
-  {
-    delay(demora);
-    encender(partida[i]);
-    delay(demora);
-    apagar(partida[i]);
-    delay(demora);
-  }  
-}
-
-
-int leerBotones()
-{
+int readButtons() {
   int flag = 0, btn;
-  while (!flag)
-  {    
-    if(digitalRead(botones[LEDROJO]) && !flag)
-    {
-      flag = 1;
-      btn = LEDROJO;
+  while (!flag) {
+    for (int i = 0; i < NUM_LEDS; i++) {
+      if (!digitalRead(buttons[i])) {  // Check for button press (active low)
+        flag = 1;
+        btn = i;
+        break;
+      }
     }
-    if(digitalRead(botones[LEDVERDE]) && !flag)
-    {
-      flag = 1;
-      btn = LEDVERDE;
-    }
-    if(digitalRead(botones[LEDAZUL]) && !flag)
-    {
-      flag = 1;
-      btn = LEDAZUL;
-    }
-    if(digitalRead(botones[LEDAMARILLO]) && !flag)
-    {
-      flag = 1;
-      btn = LEDAMARILLO;
-    }    
   }
- return btn;
+  return btn;
 }
 
-void ejecutarMelodia()
-{
-
- for (int thisNote = 0; thisNote < 8; thisNote++) 
-    {
-
-     int noteDuration = 1000 / noteDurations[thisNote];
-     tone(buzz, melody[thisNote], noteDuration);
-    
-     int pauseBetweenNotes = noteDuration * 1.30;
-     delay(pauseBetweenNotes);
-
-     noTone(buzz);
-    }
+void lightLED(int led) {
+  digitalWrite(leds[led], HIGH);
 }
 
-
-
+void turnOffLED(int led) {
+  digitalWrite(leds[led], LOW);
+}
